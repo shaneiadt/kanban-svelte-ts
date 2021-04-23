99f4c8896d0cbcdf361c93ee1294f01ae8e41e0e
Shane O'Moore <shaneiadt@gmail.com>
2021-04-23 10:51:39 +0100

    drag events

--- src/components/List/ListItem/ListItem.svelte
+++ src/components/List/ListItem/ListItem.svelte
@@ -2,7 +2,34 @@
 	import type { Column } from '../../../interfaces';
 
     export let column: Column;
-    // console.log(column);
+
+    function drag(e: Event){
+        console.log('DRAG', e.target);
+    }
+
+    function allowDrop(e: Event){
+        e.preventDefault();
+    }
+
+    function drop(e: Event){
+        e.preventDefault();
+    }
+
+    function dragEnter(e: Event, column: Column){
+        console.log('ENTER', {event:e, column});
+        const list = document.querySelector(`#${column.cssClass}-list`) as HTMLUListElement;
+        console.log(list);
+
+        list.classList.add('over');
+    }
+    
+    function dragLeave(e: Event, column: Column){
+        console.log('LEAVE', {event:e, column});
+        const list = document.querySelector(`#${column.cssClass}-list`) as HTMLUListElement;
+        console.log(list);
+
+        list.classList.remove('over');
+    }
 </script>
 
 <li class={`drag-column rounded-lg relative flex-1 overflow-x-hidden mx-1 bg-black bg-opacity-40 ${column.cssClass}-column`}>
@@ -10,9 +37,9 @@
         <h1 class="p-2">{column.title}</h1>
     </span>
     <div id={`${column.cssClass}-content`} class="custom-scroll">
-        <ul class="drag-item-list" id={`${column.cssClass}-list`}>
+        <ul class="drag-item-list" id={`${column.cssClass}-list`} on:drop={drop} on:dragover={allowDrop} on:dragenter={(e) => dragEnter(e, column)} on:dragleave={(e) => dragLeave(e, column)}>
             {#each column.items as item, i}
-                <li class="drag-item">{item.title}</li>
+                <li class="drag-item" draggable="true" on:dragstart={drag}>{item.title}</li>
             {/each}
         </ul>
     </div>
